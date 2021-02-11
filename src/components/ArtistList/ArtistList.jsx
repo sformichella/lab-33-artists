5d5ae130ae1f9c77fc0186ea06054586e07f9121
sformichella <sformichella@gmail.com>
2021-02-11 11:14:00 -0800

    ArtistList

--- src/components/ArtistList/ArtistList.jsx
+++ src/components/ArtistList/ArtistList.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+import ArtistListItem from '../ArtistListItem/ArtistListItem';
 
-export default function ArtistList() {
+export default function ArtistList({ artists }) {
+
+  const artistsList = artists.map(artist => {
+    const { id, name } = artist;
+
+    return (
+      <li key={id}>
+        <Link to={`/artist/${id}`}>
+          <ArtistListItem name={name}/>
+        </Link>
+      </li>
+    );
+  });
+
   return (
-    <div>
-      Hi! Again!
-    </div>
+    <ul>
+      { artistsList }
+    </ul>
   );
 }
+
+ArtistList.propTypes = {
+  artists: PropTypes.array.isRequired
+};

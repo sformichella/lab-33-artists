94b76685e67db30551ed1f62961e3680395c438a
sformichella <sformichella@gmail.com>
2021-02-11 10:15:09 -0800

    add router to app

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import ArtistsContainer from '../containers/ArtistsContainer/ArtistsContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route
+          path = "/"
+          exact
+          render = {routerProps => <ArtistsContainer {...routerProps}/>}
+        />
+      </Switch>
+    </Router>
+  );
 }
--- src/components/containers/ArtistsContainer/ArtistsContainer.jsx
+++ src/components/containers/ArtistsContainer/ArtistsContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ArtistList from '../../ArtistList/ArtistList';
+import ArtistsSearchForm from '../../ArtistsSearchForm/ArtistsSearchForm';
 
 export default function ArtistsContainer() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <ArtistsSearchForm/>
+      <ArtistList />
+    </>
   );
 }
